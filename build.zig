8417c9a146122893f03cb0ce3a27610c7ee0ff8b
jinzhongjia <mail@nvimer.org>
2024-07-01 21:13:45 +0800

    optimize build.zig

--- build.zig
+++ build.zig
@@ -10,85 +10,73 @@ const version = std.SemanticVersion{ .major = 0, .minor = 4, .patch = 2 };
 
 const min_zig_string = "0.13.0";
 
-const Build = blk: {
+comptime {
     const current_zig = builtin.zig_version;
     const min_zig = std.SemanticVersion.parse(min_zig_string) catch unreachable;
     if (current_zig.order(min_zig) == .lt) {
         @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ current_zig, min_zig }));
     }
-    break :blk std.Build;
-};
+}
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     // Add a global option for versioning
     const options = b.addOptions();
-    options.addOption(std.log.Level, "log_level", b.option(std.log.Level, "log_level", "The Log Level to be used.") orelse .info);
-    options.addOption(std.SemanticVersion, "zvm_version", version);
+    const log_level = b.option(std.log.Level, "log_level", "The Log Level to be used.");
+    options.addOption(std.log.Level, "log_level", log_level orelse .info);
+    options.addOption(std.SemanticVersion, "version", version);
 
+    // build the native zvm to debug
     const exe = b.addExecutable(.{
         .name = "zvm",
-        .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "src/main.zig" } },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
         .version = version,
     });
 
-    const exe_options_module = options.createModule();
-    exe.root_module.addImport("options", exe_options_module);
+    exe.root_module.addImport("options", options.createModule());
 
     b.installArtifact(exe);
 
+    // add run step
+    const run_exe = b.addRunArtifact(exe);
+    run_exe.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_exe.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
+
     const release = b.step("release", "make an upstream binary release");
     const release_targets = [_]std.Target.Query{
-        .{
-            .cpu_arch = .aarch64,
-            .os_tag = .linux,
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .os_tag = .linux,
-        },
-        .{
-            .cpu_arch = .x86,
-            .os_tag = .linux,
-        },
-        .{
-            .cpu_arch = .riscv64,
-            .os_tag = .linux,
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .os_tag = .macos,
-        },
-        .{
-            .cpu_arch = .aarch64,
-            .os_tag = .macos,
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .os_tag = .windows,
-        },
-        .{
-            .cpu_arch = .x86,
-            .os_tag = .windows,
-        },
+        .{ .cpu_arch = .aarch64, .os_tag = .linux },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux },
+        .{ .cpu_arch = .x86, .os_tag = .linux },
+        .{ .cpu_arch = .riscv64, .os_tag = .linux },
+        .{ .cpu_arch = .x86_64, .os_tag = .macos },
+        .{ .cpu_arch = .aarch64, .os_tag = .macos },
+        .{ .cpu_arch = .x86_64, .os_tag = .windows },
+        .{ .cpu_arch = .x86, .os_tag = .windows },
     };
 
+    // build the release exe
     for (release_targets) |target_query| {
         const resolved_target = b.resolveTargetQuery(target_query);
         const t = resolved_target.result;
         const rel_exe = b.addExecutable(.{
             .name = "zvm",
-            .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "src/main.zig" } },
+            .root_source_file = b.path("src/main.zig"),
             .target = resolved_target,
             .optimize = .ReleaseSafe,
             .strip = true,
         });
 
-        const rel_exe_options_module = options.createModule();
-        rel_exe.root_module.addImport("options", rel_exe_options_module);
+        rel_exe.root_module.addImport("options", options.createModule());
 
         const install = b.addInstallArtifact(rel_exe, .{});
         install.dest_dir = .prefix;
@@ -102,18 +90,13 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "src/main.zig" } },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
-    // Additional build steps for different configurations or tasks
-    // Add here as needed (e.g., documentation generation, code linting)
 }
